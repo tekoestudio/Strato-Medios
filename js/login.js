de21acc1f130b6e12854e0cf392a957ce4675b72
Hugo Moreno <hugo.moreno@tekoestudio.com>
2013-10-29 16:27:55 -0600

    Redirect

--- js/login.js
+++ js/login.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
 
 	$('#mess').hide();
 	
-	$.jStorage.remove('conectado');
+	//$.jStorage.remove('conectado');
 	
 	if($.jStorage.get("conectado"))
 		alert('si entro el store')
-		//window.location = "category.html?cat=titulares"//Redireccionamiento
+		window.location = "category.html?cat=titulares"//Redireccionamiento
 	else
 		$('html').removeClass('oculta');
 });
